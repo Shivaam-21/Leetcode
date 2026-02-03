0a1d30ed3bb794561828ca4e3e0fbc2344390194
Shivaam-21 <shiv8520am@gmail.com>
2026-02-03 22:21:08 +0530

    Add MaxConsecutiveOnes solution

---
+++ MaxConsecutiveOnes.java
@@ -0,0 +1,21 @@
+public class MaxConsecutiveOnes {
+    
+    public int longestOnes(int[] nums, int k) {
+        int maxlen=0,l=0,r=0;
+        int zero=0;
+        while(r<nums.length){
+            if(nums[r]==0) zero++;
+            while(zero>k){
+                if(nums[l]==0) zero--;
+                l++;
+            }
+            if(zero<=k) {
+                int len=r-l+1;
+                maxlen=Math.max(len,maxlen);
+            }
+            r++;
+        }
+        return maxlen;
+    }
+
+}
